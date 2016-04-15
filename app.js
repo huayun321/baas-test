ebab941f4707a22b4c04ce17c5510da8caa25849
huayun321 <huayun321@gmail.com>
2016-04-15 11:10:51 +0800

    清理 express  精简配置

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -9,32 +7,12 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-       
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-
-var auth = require('basic-auth');
-// app.use(function(request, response, next) {
-//   var user = auth(request);
-//   if (user === undefined) {
-//     console.log('User information is not available in the request ');
-//     response.statusCode = 401;
-//     response.setHeader('WWW-Authenticate', 'Basic');
-//     response.end('Unauthorized');
-//   } else {
-//     response.send(user);
-//   } });
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
